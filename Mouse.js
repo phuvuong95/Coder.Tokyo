b99f9d0438bf5dd1c3f1d5bd85bec591f12b5441
Nguyen Phu Vuong <vuongnp@ispeaking.edu.vn>
2019-09-25 09:40:42 +0700

    Add Mouse module

--- Mouse.js
+++ Mouse.js
@@ -1,4 +1,9 @@
-function Mouse(){
-	"name": "Jerry",
-	"Color" "Black"
+function Mouse(name){
+	this.name = name;
+	this.dead = false;
 }
+
+Mouse.prototype.die = function(){
+	this.
+};
+module.exports = Mouse; 
